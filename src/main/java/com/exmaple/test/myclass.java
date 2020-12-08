b55b65feccf20eaf9615a3a5c2b0b1a6ad681df1
ShekharKS <ShekharKS@RSLPTP691.rssoftware.com>
2020-12-08 13:04:20 +0530

    updated class file

--- src/main/java/com/exmaple/test/myclass.java
+++ src/main/java/com/exmaple/test/myclass.java
@@ -3,6 +3,6 @@ package com.exmaple.test;
 public class myclass {
 
 	public static void main(String[] args) {
-	    System.out.println("Hello World");
+	    System.out.println("My name is shekhar");
 	  }
 }
